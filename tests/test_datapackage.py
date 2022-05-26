1978de46585db949364d99fd92866f28abb36bd0
romainsacchi <r_s@me.com>
2022-05-26 16:49:51 +0200

    datapackage creation

---
+++ tests/test_datapackage.py
@@ -0,0 +1,11 @@
+import yaml
+from schema import And, Optional, Or, Schema, Use
+
+from premise.ecoinvent_modification import (
+    LIST_IMAGE_REGIONS,
+    LIST_REMIND_REGIONS,
+    SUPPORTED_EI_VERSIONS,
+)
+
+def test_datapackage():
+  pass

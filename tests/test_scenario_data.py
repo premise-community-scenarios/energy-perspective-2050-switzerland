00c8192f17a7d9c12bc9c7083050b42e33480c4c
romainsacchi <r_s@me.com>
2022-06-16 19:26:09 +0200

    datapackage creation

--- tests/test_scenario_data.py
+++ tests/test_scenario_data.py
@@ -6,72 +6,92 @@ from premise.ecoinvent_modification import (
     LIST_REMIND_REGIONS,
     SUPPORTED_EI_VERSIONS,
 )
+from premise.geomap import Geomap
 
 from datapackage import Package
 
 FILEPATH_DATAPACKAGE_SCHEMA = "./datapackage.json"
 dp = Package(FILEPATH_DATAPACKAGE_SCHEMA)
 
-for resource in dp.resources:
+resource = dp.get_resource("config")
+with open(resource.source, "r") as stream:
+    config_file = yaml.safe_load(stream)
 
+resource = dp.get_resource("scenario_data")
+scenario_data = resource.read()
+scenario_headers = resource.headers
 
-    if resource.name == "config":
-        with open(resource.descriptor["path"], "r") as stream:
-            config_file = yaml.safe_load(stream)
-
-    if resource.name == "scenario_data":
-        df = pd.read_csv(resource.descriptor["path"])
+df = pd.DataFrame(scenario_data, columns=scenario_headers)
 
 
 def test_scenario_data_file():
-
     mandatory_fields = ["model", "pathway", "region", "variables", "unit"]
     if not all(v in df.columns for v in mandatory_fields):
         raise ValueError(
             f"One or several mandatory column are missing "
-            f"in the scenario_data file. Mandatory columns: {mandatory_fields}."
+            f"in the scenario data file. Mandatory columns: {mandatory_fields}."
         )
 
-    years_cols = [c for c in df.columns if isinstance(c, int)]
-    if any(y for y in years_cols if y < 2005 or y > 2100):
+    years_cols = []
+    for h, header in enumerate(scenario_headers):
+        try:
+            years_cols.append(int(header))
+        except ValueError:
+            continue
+
+    if not all(2005 <= y <= 2100 for y in years_cols):
         raise ValueError(
-            f"One or several of the years provided in the scenario_data file no. {i + 1} are "
+            f"One or several of the years provided in the scenario data file are "
             "out of boundaries (2005 - 2100)."
         )
 
+
     if len(pd.isnull(df).sum()[pd.isnull(df).sum() > 0]) > 0:
         raise ValueError(
-            f"The following columns in the scenario_data file"
+            f"The following columns in the scenario data file"
             f"contains empty cells.\n{pd.isnull(df).sum()[pd.isnull(df).sum() > 0]}."
         )
 
 
-
     d_regions = {"remind": LIST_REMIND_REGIONS, "image": LIST_IMAGE_REGIONS}
 
+    list_ei_locs = [
+        i if isinstance(i, str) else i[-1]
+        for i in list(Geomap(model="remind").geo.keys())
+    ]
+
     for irow, r in df.iterrows():
-        if r["region"] not in d_regions[r["model"]]:
+        if (
+                r["region"] not in d_regions[r["model"]]
+                and r["region"] not in list_ei_locs
+        ):
             raise ValueError(
                 f"Region {r['region']} indicated "
-                f"in row {irow} is not valid for model {r['model'].upper()}."
+                f"in row {irow} is not a valid region for model {r['model'].upper()}"
+                f"and is not found within ecoinvent locations."
             )
 
     if not all(
-        v in get_recursively(config_file, "variable")
-        for v in df["variables"].unique()
+            v in get_recursively(config_file, "variable")
+            for v in df["variables"].unique()
     ):
         raise ValueError(
-            f"One or several variable names in the scenario_data file "
-            "cannot be found in the configuration_file."
+            f"One or several variable names in the scenario data file "
+            "cannot be found in the configuration file."
         )
 
     if not all(
-        v in df["variables"].unique()
-        for v in get_recursively(config_file, "variable")
+            v in df["variables"].unique()
+            for v in get_recursively(config_file, "variable")
     ):
+        missing_variables = [
+            v
+            for v in get_recursively(config_file, "variable")
+            if v not in df["variables"].unique()
+        ]
         raise ValueError(
-            f"One or several variable names in the configuration_file "
-            "cannot be found in the scenario_data file."
+            f"One or several variable names in the configuration file "
+            f"cannot be found in the scenario data file: {missing_variables}."
         )
 
     try:
@@ -79,7 +99,7 @@ def test_scenario_data_file():
     except ValueError as e:
         raise TypeError(
             f"All values provided in the time series must be numerical "
-            f"in the scenario_data file."
+            f"in the scenario data file."
         ) from e
 
 
